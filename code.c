6fe70d405bf16a01b6ffb774d37c592905281e31
kolesnikstanislav <162875948+kolesnikstanislav@users.noreply.github.com>
2024-06-10 11:37:43 +0300

    code is updated in test entry method

--- code.c
+++ code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 int count_unique_elements_divisible_by_k(int arr[], int size, int k) {
     int unique_elements[1000] = {0}; // Предполагаем, что максимальное значение элемента в массиве не превышает 1000
@@ -23,13 +24,37 @@ int count_unique_elements_divisible_by_k(int arr[], int size, int k) {
     return count;
 }
 
-int main() {
+void test_allElements(){
     int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     int size = sizeof(arr) / sizeof(arr[0]);
     int k = 10;
 
     int result = count_unique_elements_divisible_by_k(arr, size, k);
-    printf("Количество уникальных элементов, делящихся на %d: %d\n", k, result);
+    assert(result == 10);
+}
+
+void test_noNeedElements(){
+    int arr[] = {7, 9, 11, 13, 17, 19};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int k = 5;
+
+    int result = count_unique_elements_divisible_by_k(arr, size, k);
+    assert(result == 0);
+}
+
+void test_severalElements(){
+    int arr[] = {3, 7, 9, 11, 13, 17, 19};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    int result = count_unique_elements_divisible_by_k(arr, size, k);
+    assert(result == 2);
+}
+
+int main() {
+    test_allElements();
+    test_noNeedElements();
+    test_severalElements();
 
     return 0;
 }
